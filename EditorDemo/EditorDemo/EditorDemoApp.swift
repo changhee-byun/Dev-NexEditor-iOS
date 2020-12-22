7a9c254a4804b7d16a1afc28c519f4f95e70706c
Changhee Byun <changhee.byun@kinemaster.com>
2020-12-22 12:23:59 +0900

    add new demo ui app project

---
+++ EditorDemo/EditorDemo/EditorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditorDemoApp.swift
+//  EditorDemo
+//
+//  Created by CHANGHEE2 on 2020/12/22.
+//
+
+import SwiftUI
+
+@main
+struct EditorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
