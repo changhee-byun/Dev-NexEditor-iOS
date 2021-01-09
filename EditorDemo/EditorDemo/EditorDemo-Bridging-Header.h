976c4b91c6ae3f89c3fc3306e36f043b722fc5cd
Changhee Byun <changhee.byun@kinemaster.com>
2021-01-09 12:21:32 +0900

    remove unused bridging-header files.

--- EditorDemo/EditorDemo/EditorDemo-Bridging-Header.h
+++ EditorDemo/EditorDemo/EditorDemo-Bridging-Header.h
@@ -8,13 +8,13 @@
 #ifndef EditorDemo_Bridging_Header_h
 #define EditorDemo_Bridging_Header_h
 
-#import "NXEEngine.h"
-#import "NXELayerEditorView.h"
-#import "NXEClip.h"
-#import "NXEProject.h"
-#import "NexEditorLog.h"
-#import "NXEAssetLibrary.h"
-#import "NXEBeatTemplateProject.h"
-#import "NXEAssetItem.h"
+//#import "NXEEngine.h"
+//#import "NXELayerEditorView.h"
+//#import "NXEClip.h"
+//#import "NXEProject.h"
+//#import "NexEditorLog.h"
+//#import "NXEAssetLibrary.h"
+//#import "NXEBeatTemplateProject.h"
+//#import "NXEAssetItem.h"
 
 #endif /* EditorDemo_Bridging_Header_h */
